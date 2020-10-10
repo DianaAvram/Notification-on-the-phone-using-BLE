7f6c4f7f4dc12470c938dec1d038c1413aebbb57
unknown <avram.diana95@gmail.com>
2020-10-10 15:12:43 +0200

    First commit

---
+++ nRF5_SDK_1530/examples/ble_peripheral/ble_app_blinky/pca10056/s140/ses/Output/ble_app_blinky_pca10056_s140 Release/Obj/ses_startup_nrf52840.d
@@ -0,0 +1,2 @@
+Output/ble_app_blinky_pca10056_s140\ Release/Obj/ses_startup_nrf52840.o: \
+ C:\SESProj\nRF5_SDK_1530\modules\nrfx\mdk\ses_startup_nrf52840.s
---
+++ nRF5_SDK_1530/examples/ble_peripheral/ble_app_blinky/pca10056/s140/ses/Output/ble_app_blinky_pca10056_s140 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_blinky_pca10056_s140\ Release/Obj/utf.o: \
+ C:\SESProj\nRF5_SDK_1530\external\utf_converter\utf.c \
+ C:\SESProj\nRF5_SDK_1530\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h
